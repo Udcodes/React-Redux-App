fcc8819038d472da60078d14f37c77726d7014fc
Kate Efimova <kate.efimova0@gmail.com>
2019-09-12 14:51:02 -0700

    Fetching and functionality, working on visuals

--- src/components/quoteComponent.js
+++ src/components/quoteComponent.js
@@ -1,4 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
-
 import { getQuote } from "../actions/actions";
+import iconSVG from "../../public/images/redo-solid.svg";
+
+const QuoteComponent = props => {
+  return (
+    <div className="card">
+      <div className="top"></div>
+      <div className="bottom">
+        <button onClick={() => props.getQuote()}>
+          <img src={iconSVG} alt="A redo icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default connect(
+  null,
+  { getQuote }
+)(QuoteComponent);
